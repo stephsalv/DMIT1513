b9a8b5f700e0d28671c28915d04aafbe88942309
Stephen Salvador <albertstephenhere@gmail.com>
2026-01-21 00:33:53 -0700

    Update

--- StephenSalvadorGameMenus/Assets/Scripts/Checkpoint.cs
+++ StephenSalvadorGameMenus/Assets/Scripts/Checkpoint.cs
@@ -6,50 +6,44 @@ public class Checkpoint : MonoBehaviour
     public Checkpoint[] checkpoints;
     public FinalCheckpoint finalCheckpoint;
 
-    private bool triggered = false;
+    //private bool triggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (triggered) return;
-
-        if (other.CompareTag("Player"))
-        {
-            triggered = true;
-            Debug.Log($"Player hit checkpoint {index}");
-
-            // Disable this checkpoint
-            gameObject.SetActive(false);
-
-            // If this is the last checkpoint, enable FinalCheckpoint
-            if (index == checkpoints.Length - 1)
+        //if (triggered)
+        //{
+            if (other.CompareTag("Player"))
             {
-                finalCheckpoint.gameObject.SetActive(true);
-            }
-            else
-            {
-                // Enable next checkpoint
-                checkpoints[index + 1].gameObject.SetActive(true);
-            }
-        }
-        else if (other.CompareTag("AICar"))
-        {
-            triggered = true;
-            Debug.Log($"AICar hit checkpoint {index}");
+                //triggered = true;
+                Debug.Log($"Player hit checkpoint {index}");
 
-            // Disable this checkpoint
-            gameObject.SetActive(false);
+                gameObject.SetActive(false);
 
-            // If this is the last checkpoint, enable FinalCheckpoint
-            if (index == checkpoints.Length - 1)
-            {
-                finalCheckpoint.gameObject.SetActive(true);
+                if (index == checkpoints.Length - 1)
+                {
+                    finalCheckpoint.gameObject.SetActive(true);
+                }
+                else
+                {
+                    checkpoints[index + 1].gameObject.SetActive(true);
+                }
             }
-            else
+            else if (other.CompareTag("AICar"))
             {
-                // Enable next checkpoint
-                checkpoints[index + 1].gameObject.SetActive(true);
+                //triggered = true;
+                Debug.Log($"AICar hit checkpoint {index}");
+
+                if (index == checkpoints.Length - 1)
+                {
+                    finalCheckpoint.gameObject.SetActive(true);
+                }
+                else
+                {
+                    checkpoints[index + 1].gameObject.SetActive(true);
+                }
             }
-        }
+        //    return;
+        //}
     }
 }
 
