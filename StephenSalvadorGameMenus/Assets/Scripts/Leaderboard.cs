335aac9e91ac462bf518f1630d013e6b9e428e99
Stephen Salvador <albertstephenhere@gmail.com>
2026-01-26 19:24:00 -0700

    fixed leaderboard script

--- StephenSalvadorGameMenus/Assets/Scripts/Leaderboard.cs
+++ StephenSalvadorGameMenus/Assets/Scripts/Leaderboard.cs
@@ -1,22 +1,39 @@
-using UnityEngine;
-using UnityEngine.UI;
+using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
-    [Header("UI Elements")]
-    public GameObject[] gameObjects;
-    public TextMeshProUGUI[] playerName;
-    public TextMeshProUGUI[] raceTime;
-    public TextMeshProUGUI[] vehicleType;
+    public SaveSystem saveSystem;
+
+    public TextMeshProUGUI[] placementTexts;
 
     private void Start()
     {
-        ShowLeaderboard();
+        RefreshLeaderboard();
     }
 
-    private void ShowLeaderboard()
+    public void RefreshLeaderboard()
     {
+        if (saveSystem == null || placementTexts == null || placementTexts.Length == 0)
+            return;
 
+        List<SaveProfile> profiles = saveSystem.LoadAllSaveData();
+        profiles.RemoveAll(p => p.bestTime <= 0f);
+        profiles.Sort((a, b) => a.bestTime.CompareTo(b.bestTime));
+
+        for (int i = 0; i < placementTexts.Length; i++)
+        {
+            if (i < profiles.Count)
+            {
+                SaveProfile p = profiles[i];
+                placementTexts[i].text = $"{i + 1}. {p.profileName} {p.vehicleName} {p.bestTime:F2}s";
+            }
+            else
+            {
+                placementTexts[i].text =
+                    $"{i + 1}. ---\n---\n--.--";
+            }
+        }
     }
 }
